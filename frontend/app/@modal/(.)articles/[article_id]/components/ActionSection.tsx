6f50fa3150358d389c712e43230c2785cacd7aec
{cocoide} <cocoide@gmail.com>
2023-08-12 16:49:12 +0900

    Update handleBookmark

--- frontend/app/@modal/(.)articles/[article_id]/components/ActionSection.tsx
+++ frontend/app/@modal/(.)articles/[article_id]/components/ActionSection.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { collectionDialogAtom } from '@/stores/dialog'
+import { useAuth } from '@/hooks/useAuth'
+import { collectionDialogAtom, loginDialogAtom } from '@/stores/dialog'
 import { BookmarkIcon, HandThumbDownIcon, HandThumbUpIcon } from '@heroicons/react/24/outline'
 import { useAtom } from 'jotai'
 
@@ -7,7 +8,17 @@ interface Props {
     articleId: number
 }
 export default function ActionSection({ articleId }: Props) {
-    const [_, setCollectionDialogOpen] = useAtom(collectionDialogAtom)
+    const [_, setLoginOpen] = useAtom(loginDialogAtom)
+    const { status } = useAuth()
+    const [__, setCollectionDialogOpen] = useAtom(collectionDialogAtom)
+    function handleBookmark() {
+        if (status === "unauthenticated") {
+            setCollectionDialogOpen(articleId)
+        }
+        if (status === "authenticated") {
+            setCollectionDialogOpen(articleId)
+        }
+    }
     return (
         <div className="w-full flex flex-row items-center  space-x-3">
             <div className="flex items-center rounded-full ring-1 ring-gray-100 bg-gray-100
@@ -15,7 +26,7 @@ export default function ActionSection({ articleId }: Props) {
                 <HandThumbUpIcon className='h-9 w-9 text-gray-500 hover:text-pink-300 hover:bg-pink-100 rounded-full p-1' />
                 <HandThumbDownIcon className='h-9 w-9 text-gray-500 p-1' />
             </div>
-            <button onClick={() => setCollectionDialogOpen(articleId)}
+            <button onClick={handleBookmark}
                 className="flex items-center rounded-full ring-1 ring-gray-100 bg-gray-100
             justify-center p-1">
                 <BookmarkIcon className='h-6 w-6 text-gray-500' />
