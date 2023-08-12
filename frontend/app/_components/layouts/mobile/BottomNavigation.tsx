9985fdab4aae32b51cb03909d4231f9870d137c9
{cocoide} <cocoide@gmail.com>
2023-08-12 16:42:25 +0900

    Fixed navigation error

--- frontend/app/_components/layouts/mobile/BottomNavigation.tsx
+++ frontend/app/_components/layouts/mobile/BottomNavigation.tsx
@@ -5,9 +5,11 @@ import { HomeIcon, MagnifyingGlassIcon, NewspaperIcon, PlusCircleIcon, UserCircl
 import { useAtom } from 'jotai'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function BottomNavigation() {
   const { user, status } = useAuth()
+  const router = useRouter()
   const [_, setIsPostDialogOpen] = useAtom(postDialogAtom)
   const [__, setIsLoginDialogOpen] = useAtom(loginDialogAtom)
   const uid = user.uid
@@ -25,10 +27,18 @@ export default function BottomNavigation() {
       setIsPostDialogOpen(true)
     }
   }
+  function handleOnFeedPage() {
+    if (status === "unauthenticated") {
+      setIsLoginDialogOpen(true)
+    }
+    if (status === "authenticated") {
+      router.push("/feed")
+    }
+  }
   return (
     <div className="w-full flex flex-row items-center justify-between px-[10%] py-2 bg-white/80 backdrop-blur-[5px] text-slate-500">
       <Link href={"/"} ><HomeIcon className='h-7 w-7' /></Link>
-      <Link href={"/feed"} ><NewspaperIcon className='h-7 w-7' onClick={handleAuth} /></Link>
+      <button onClick={handleOnFeedPage} ><NewspaperIcon className='h-7 w-7' onClick={handleAuth} /></button>
       <button onClick={handlePost}><PlusCircleIcon className='h-7 w-7' /></button>
       <Link href={"/explore"} ><MagnifyingGlassIcon className='h-7 w-7' /></Link>
       <Link href={AccountURL} onClick={handleAuth}>
