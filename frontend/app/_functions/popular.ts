c0959a52a356e077fcfec676283d06a401f083f8
{cocoide} <cocoide@gmail.com>
2023-08-12 13:59:56 +0900

    Update fetch style of popular list

--- frontend/app/_functions/popular.ts
+++ frontend/app/_functions/popular.ts
@@ -3,9 +3,9 @@ import { Source, Topic } from '@/types/model';
 
 export const popularAPI = {
     async GetPopularTopics() {
-        return await api.get<Topic[]>("/topic/popular", 24 * 60 * 60)
+        return await api.get<Topic[]>("/topic/popular", "no-store")
     },
     async GetPopularSources() {
-        return await api.get<Source[]>("/source/popular", 24 * 60 * 60)
+        return await api.get<Source[]>("/source/popular", "no-store")
     }
 }
