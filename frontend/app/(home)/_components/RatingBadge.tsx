47ccc8ddc74cc0669e60e58f163ea680f988afe3
{cocoide} <cocoide@gmail.com>
2023-10-05 01:12:04 +0900

    style

--- frontend/app/(home)/_components/RatingBadge.tsx
+++ frontend/app/(home)/_components/RatingBadge.tsx
@@ -8,18 +8,18 @@ export const RatingBadge = ({ count }: { count: number }) => {
 
     switch (true) {
         case count >= 1000:
-            text_style = "text-white bg-yellow-500 font-bold rounded-xl"
+            text_style = "text-yellow-300  dark:text-yellow-100 font-bold"
             icon = <TrophyIcon className='h-5 w-5' />
             break;
         case 999 >= count && count >= 100:
-            text_style = "text-red-500 font-bold p-[2px]"
+            text_style = "text-red-300 dark:text-red-200 font-bold"
             icon = <FireIcon className='h-5 w-5' />
             break;
         case 99 >= count && count >= 50:
-            text_style = "text-slate-500 dark:text-slate-100 font-bold"
+            text_style = "text-slate-300 dark:text-slate-100 font-bold"
             break;
         default:
-            text_style = "text-slate-400 dark:text-slate-100"
+            text_style = "text-slate-300 dark:text-slate-100 font-bold"
     }
     return (
         <div className={clsx("flex flex-row items-center space-2 p-[2px]", text_style)}>
