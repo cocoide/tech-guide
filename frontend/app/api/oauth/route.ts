82083f5840fb4ead2a15c4d2116ad52f9eeaf2e1
{cocoide} <cocoide@gmail.com>
2023-10-22 20:35:15 +0900

    fix: api route internal error

--- frontend/app/api/oauth/route.ts
+++ frontend/app/api/oauth/route.ts
@@ -1,10 +1,12 @@
-import { cookies } from "next/headers";
 import { NextResponse } from 'next/server';
-import {NextRequest} from "next/server.js";
+import {NextRequest} from "next/server";
+
 export async function GET(request: NextRequest) {
     const token=request.nextUrl.searchParams.get("token")
+
+    const response = NextResponse.redirect("https://www.tech-guide.jp/")
     if (token){
-    cookies().set({
+   response.cookies.set({
         name: 'accessToken',
         domain: '.tech-guide.jp',
         value: token,
@@ -13,5 +15,5 @@ export async function GET(request: NextRequest) {
         path: '/',
     })
     }
-    return NextResponse.redirect("www.tech-guide.jp/")
+    return response
 }
