e093adca025d608a0078a440a4ace8a81dfd3b24
{cocoide} <cocoide@gmail.com>
2023-10-23 15:05:06 +0900

    feat: get cookie api route

--- frontend/app/api/oauth/route.ts
+++ frontend/app/api/oauth/route.ts
@@ -1,19 +1,26 @@
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
-    const token=request.nextUrl.searchParams.get("token")
+export async function POST(request: NextRequest) {
+    const token = request.nextUrl.searchParams.get("token")
 
     const response = NextResponse.redirect("https://www.tech-guide.jp")
-    if (token){
-   response.cookies.set({
-        name: 'accessToken',
-        domain: '.tech-guide.jp',
-        value: token,
-        httpOnly: true,
-       sameSite: 'none',
-        path: '/',
-       secure: true,
-    })
+    if (token) {
+        response.cookies.set({
+            name: 'accessToken',
+            domain: '.tech-guide.jp',
+            value: token,
+            httpOnly: true,
+            sameSite: 'lax',
+            path: '/',
+            secure: true,
+        })
     }
     return response
+}
+
+export async function GET() {
+    const cookieStore = cookies()
+    const token = cookieStore.get("accessToken")
+    return NextResponse.json(token, { status: 200 })
 }
