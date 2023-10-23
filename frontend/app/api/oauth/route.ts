abfebc6c081308f771f8a0012e33b1be860cbcd1
{cocoide} <cocoide@gmail.com>
2023-10-23 10:32:07 +0900

    fix: oauth api route

--- frontend/app/api/oauth/route.ts
+++ frontend/app/api/oauth/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from 'next/server';
-import {NextRequest} from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
     const token=request.nextUrl.searchParams.get("token")
 
-    const response = NextResponse.redirect("https://www.tech-guide.jp/")
+    const response = NextResponse.redirect("https://www.tech-guide.jp")
     if (token){
    response.cookies.set({
         name: 'accessToken',
         domain: '.tech-guide.jp',
         value: token,
         httpOnly: true,
-        sameSite: 'none',
+       sameSite: 'none',
         path: '/',
+       secure: true,
     })
     }
     return response
