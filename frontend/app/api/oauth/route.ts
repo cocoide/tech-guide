982b9cf1f12c0d5a2d940e6c5e5da785e5bb3321
{cocoide} <cocoide@gmail.com>
2023-10-24 00:58:48 +0900

    feat: set refreshToken

--- frontend/app/api/oauth/route.ts
+++ frontend/app/api/oauth/route.ts
@@ -7,24 +7,36 @@ const corsHeaders = {
 }
 
 export async function GET(request: NextRequest) {
-    const token = request.nextUrl.searchParams.get("token")
+    const accessToken = request.nextUrl.searchParams.get("access")
+    const refreshToken = request.nextUrl.searchParams.get("refresh")
     const response = NextResponse.redirect("https://www.tech-guide.jp")
     response.headers.set('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_API_BASE_URL)
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    if (token === null || token === "") {
-        console.log("Failed to get token")
+    if (!accessToken||!refreshToken) {
+        console.log("Failed to get tokens")
         return response
     }
         response.cookies.set({
             name: 'accessToken',
             domain: '.tech-guide.jp',
-            value: token,
+            value: accessToken,
             httpOnly: true,
             sameSite: 'lax',
-            maxAge: 60 * 60,// 1時間,
+            maxAge: 60 * 60 * 24 * 7, //7日
             path: '/',
             secure: true,
         })
+
+    response.cookies.set({
+        name: 'refreshToken',
+        domain: '.tech-guide.jp',
+        value: refreshToken,
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24 * 30,// 30日
+        path: '/',
+        secure: true,
+    })
 
     return response
 }
