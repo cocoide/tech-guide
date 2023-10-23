fb006071f73b6e7e32aef1490e4b09f2828ac054
{cocoide} <cocoide@gmail.com>
2023-10-23 22:15:19 +0900

    feat: extract account_id from jwt in front

--- frontend/app/_server_functions/auth.ts
+++ frontend/app/_server_functions/auth.ts
@@ -1,12 +1,47 @@
-import { cookies } from 'next/headers'
+import jwt, { VerifyOptions } from 'jsonwebtoken';
+import { cookies } from 'next/headers';
 
 export const authServerFunc = {
     async GetAuth() {
         'use server'
+        var token: string = ''
+        var account_id: number = 0
+        try {
         const cookieStore = cookies()
         const cookie = cookieStore.get('accessToken')
+            if (cookie?.value) {
+                token = cookie.value
+                const claims = verifyToken(token)
+                if (claims) {
+                    account_id = claims
+                }
+            }
+        } catch (e) {
+            console.log(e)
+        }
         return {
-            token: cookie?.value,
+            token: token,
+            account_id: account_id
         }
     }
+}
+
+
+function verifyToken(token: string): number | null {
+    const option: VerifyOptions = {
+        algorithms: ['HS256'],
+    }
+    const decoded = jwt.decode(token, option)
+    if (decoded === null || typeof decoded === 'string' || !decoded.exp) {
+        return null
+    }
+    if (Date.now() < decoded.exp * 1000) {
+        // Add refresh token later
+        console.log("expire")
+    }
+    const account_id = decoded["account_id"] as number
+    if (!account_id) {
+        return null
+    }
+    return account_id
 }
