85f0334306191304183d6046448b52b7255db6b1
{cocoide} <cocoide@gmail.com>
2023-10-25 12:11:50 +0900

    style: update feed page loading

--- frontend/app/feed/layout.tsx
+++ frontend/app/feed/layout.tsx
@@ -1,13 +1,23 @@
+import { NewspaperIcon } from '@heroicons/react/24/outline'
+import TopicDialogButton from '../_components/layouts/button/FeedFileterDialogButton'
+import SectionHeader from '../_components/layouts/desktop/SectionHeader'
 
 interface Props {
     children: React.ReactNode
 }
 export default function FeedLayout({ children }: Props) {
     return (
-        <>
-            <div className="flex flex-row relative">
-                {children}
+
+        <div className="flex flex-col w-full pb-10 relative">
+            <div className="sticky top-0 h-12 bg-white/70 dark:bg-black/30 dark:text-slate-300 backdrop-blur-[5px] z-20">
+                <SectionHeader
+                    titleItem={<div className='custom-badge text-gray-500'><NewspaperIcon className='h-5 w-5' /><div>フィード</div></div>}
+                    rightItem={
+                        <TopicDialogButton />
+                    }
+                />
             </div>
-        </>
+                {children}
+        </div>
     )
 }
---
+++ frontend/app/feed/loading.tsx
@@ -0,0 +1,11 @@
+import LoaderArticleCard from '@/app/(home)/_components/LoaderArticleCard';
+
+export default function Loading() {
+    return (
+        <div className="w-full grid sm:grid-cols-2 xl:grid-cols-3 gap-2 p-[20px]">
+            {Array(10).fill(null).map((_, index) => (
+                <LoaderArticleCard key={index + "loader"} />
+            ))}
+        </div>
+    )
+}
