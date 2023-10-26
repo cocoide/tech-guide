4f393320aa1a75d359a1430628bc73ec5fe3c780
{cocoide} <cocoide@gmail.com>
2023-10-26 11:03:19 +0900

    debug

--- frontend/app/api/session/route.ts
+++ frontend/app/api/session/route.ts
@@ -1,15 +1,15 @@
 import { api } from '@/app/_functions/API';
 import { AccountSession } from '@/types/model';
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { serverAuthFunc } from '../../_server_actions/auth';
 
 export async function GET() {
     try {
-        const token = await serverAuthFunc.GetAccessToken()
-        if (token) {
+        var accessToken = cookies().get("accessToken")?.value
+        if (accessToken) {
             throw new Error(`Error getting token`)
         }
-        const { data: session, error } = await api.get<AccountSession>("/account/session", "no-store", token)
+        const { data: session, error } = await api.get<AccountSession>("/account/session", "no-store", accessToken)
         if (!session || error) {
             throw new Error(`Error getting session: ${error}`)
         }
