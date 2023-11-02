9770d77088a1dd9880a286d2c0f4fb5668cbab3d
{cocoide} <cocoide@gmail.com>
2023-11-02 20:39:13 +0900

    fix: server function error

--- frontend/app/feed/_functions/feed.ts
+++ frontend/app/feed/_functions/feed.ts
@@ -3,17 +3,19 @@
 import { api } from '@/app/_functions/API';
 import { serverAuthFunc } from '@/app/_server_actions/auth';
 import { Article } from '@/types/model';
+import {cookies} from "next/headers";
+import {decodeJwt, refreshAccessToken} from "@/utils/jwt";
 
 export async function FetchFeed(page = 1): Promise<Article[]> {
     "use server"
+
     var articles: Article[] = [];
 
-    const token = await serverAuthFunc.GetAccessToken()
-    if (!token) {
-        console.log("Unauthorized")
-        return articles
+    var accessToken = cookies().get("accessToken")?.value
+    if (!accessToken) {
+        throw new Error(`Error getting token`)
     }
-    const { data, error } = await api.get<Article[]>(`/account/feeds?page=${page}`, "no-store", token);
+    const { data, error } = await api.get<Article[]>(`/account/feeds?page=${page}`, "no-store", accessToken);
     console.log(error)
     if (data) {
         articles = data
