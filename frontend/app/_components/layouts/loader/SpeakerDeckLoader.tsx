e034adcd582309c77be60c23f252ecc2facf84d7
{cocoide} <cocoide@gmail.com>
2023-11-08 11:59:12 +0900

    fix: build error

---
+++ frontend/app/_components/layouts/loader/SpeakerDeckLoader.tsx
@@ -0,0 +1,7 @@
+const SpeakerDeckLoader = () => {
+    return (
+        <div className="w-[100%] md:h-[300px] lg:h-[400px] rounded-xl bg-gray-100
+    animate-pulse"></div>
+    )
+}
+export default SpeakerDeckLoader
--- frontend/app/articles/[article_id]/components/RelatedArticles.tsx
+++ frontend/app/articles/[article_id]/components/RelatedArticles.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function RelatedArticles({ articles, origin }: Props) {
     return (
-        <div className='flex flex-col space-y-3 bg-slate-50 rounded-xl p-3'>
+        <div className='flex flex-col space-y-3 rounded-xl p-3'>
             <div className="text-slate-600">関連記事</div>
             {articles.map(article => {
                 return <ArticleCard key={article.id} article={article} origin={origin} />
