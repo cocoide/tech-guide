48ec410e94c1ebadb9dedcc36d738665bc56e794
{cocoide} <cocoide@gmail.com>
2023-11-08 14:56:46 +0900

    fix: loading event

--- frontend/app/articles/[article_id]/components/OutlineDialog.tsx
+++ frontend/app/articles/[article_id]/components/OutlineDialog.tsx
@@ -11,23 +11,21 @@ import remarkGfm from 'remark-gfm'
 
 const OutlineDialog = () => {
     const [dialogState, _] = useAtom(outlineDialogAtom)
-    const { data: overview, isLoading } = useQuery({
+    const { data: overview, isLoading, isFetching } = useQuery({
         queryFn: async () => (await articleAPI.GetOverview(dialogState)).data,
         queryKey: [`article_outline.${dialogState}`],
         enabled: typeof dialogState !== 'boolean',
     })
-    if (!overview) {
-        return null
-    }
     return (
-        <CustomDialog layout='my-[150px] bg-white z-50 sm:mx-[15%] md:my-[100px] md:mx-[20%] lg:mx-[25%] sm:rounded-xl'
+        <CustomDialog layout='my-[100px] bg-white z-50 sm:mx-[15%] md:my-[80px] md:mx-[20%] lg:mx-[25%] sm:rounded-xl overflow-y-hidden'
             openAtom={outlineDialogAtom}
             content={
-                <div className="relative flex flex-col  p-2 space-y-2">
+                <div className="relative flex flex-col  p-2 space-y-2 ">
                     <div className="text-gray-500">Outlines</div>
                     {isLoading ?
                         <OutlineLoader />
                         :
+                        overview && isFetching &&
                         <ReactMarkdown remarkPlugins={[remarkGfm]} className='markdown'
                         >{overview}</ReactMarkdown>
                     }
