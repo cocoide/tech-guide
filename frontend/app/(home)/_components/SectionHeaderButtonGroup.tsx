33cfdcc538376008d34bc101cf7c4c7690c691a9
{cocoide} <cocoide@gmail.com>
2023-11-10 08:01:31 +0900

    style(FE)

--- frontend/app/(home)/_components/SectionHeaderButtonGroup.tsx
+++ frontend/app/(home)/_components/SectionHeaderButtonGroup.tsx
@@ -9,15 +9,27 @@ const SectionHeaderButtonGroup = () => {
     const searchParams = useSearchParams()
     const order = searchParams.get("order")
     const router = useRouter()
+    function handleLatest() {
+        router.push('/')
+        router.refresh()
+    }
+    function handleTrend() {
+        router.push('/?order=trend')
+        router.refresh()
+    }
+    function handleDiscuss() {
+        router.push('/?order=discuss')
+        router.refresh()
+    }
     return (
         <WStack centerX={true} className='space-x-3 w-full'>
-            <button onClick={() => router.push('/')} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', !order ? 'bg-gray-100 dark:bg-gray-800' : '')}>
+            <button onClick={handleLatest} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', !order ? 'bg-gray-100 dark:bg-gray-800' : '')}>
                 <HomeIcon className='h-5 w-5' /><div className='text-sm'>Latest</div>
             </button>
-            <button onClick={() => router.push('/?order=trend')} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', order === "trend" ? "bg-gray-100 dark:bg-gray-800" : "")}>
+            <button onClick={handleTrend} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', order === "trend" ? "bg-gray-100 dark:bg-gray-800" : "")}>
                 <FireIcon className='h-5 w-5' /><div className='text-sm'>Trend</div>
             </button>
-            <button onClick={() => router.push('/?order=discuss')} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', order === "discuss" ? "bg-gray-100 dark:bg-gray-800" : "")}>
+            <button onClick={handleDiscuss} className={clsx('custom-badge custom-text custom-border p-1 rounded-xl', order === "discuss" ? "bg-gray-100 dark:bg-gray-800" : "")}>
                 <ChatBubbleOvalLeftEllipsisIcon className='h-5 w-5' /><div className='text-sm'>Discuss</div>
             </button>
         </WStack>
