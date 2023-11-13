cb70ab4fc9dc0badc309468f610bc42e01230e4a
{cocoide} <cocoide@gmail.com>
2023-11-13 10:25:22 +0900

    style(FE)

--- frontend/app/_components/loaders/RelatedArticlesLoader.tsx
+++ frontend/app/_components/loaders/RelatedArticlesLoader.tsx
@@ -2,7 +2,7 @@ import LoaderArticleCard from '@/app/(home)/_components/LoaderArticleCard'
 
 export const RelatedArticlesLoader = () => {
     return (
-        <div className='grid sm:grid-cols-2 gap-3 p-3'>
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-3 p-3'>
             {Array(4).fill(null).map((_, index) => (
                 <LoaderArticleCard key={index + "loader"} />
             ))}
