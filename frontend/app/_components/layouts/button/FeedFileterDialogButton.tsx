19650a61ef33004990e6bd962932d9afaa063154
{cocoide} <cocoide@gmail.com>
2023-11-13 17:31:51 +0900

    style(FE)

--- frontend/app/_components/layouts/button/FeedFileterDialogButton.tsx
+++ frontend/app/_components/layouts/button/FeedFileterDialogButton.tsx
@@ -7,8 +7,9 @@ import { useAtom } from 'jotai'
 const FeedFileterDialogButton = () => {
     const [__, setTopicDialogOpen] = useAtom(topicDialogAtom)
     return (
-        <button onClick={() => setTopicDialogOpen(true)} className="text-gray-500">
+        <button onClick={() => setTopicDialogOpen(true)} className="custom-text custom-badge">
             <AdjustmentsHorizontalIcon className='h-7 w-7' />
+            <div className="text-sm">設定</div>
         </button>
     )
 }
--- frontend/app/feed/layout.tsx
+++ frontend/app/feed/layout.tsx
@@ -9,9 +9,9 @@ export default function FeedLayout({ children }: Props) {
     return (
 
         <div className="flex flex-col w-full pb-10 relative">
-            <div className="sticky top-0 h-12 bg-white/70 dark:bg-black/30 dark:text-slate-300 backdrop-blur-[5px] z-20">
+            <div className="sticky top-0 h-12 bg-white/70 dark:bg-black/30  backdrop-blur-[5px] z-20 custom-text">
                 <SectionHeader
-                    titleItem={<div className='custom-badge text-gray-500'><NewspaperIcon className='h-5 w-5' /><div>フィード</div></div>}
+                    titleItem={<div className='custom-badge text-gray-500'><NewspaperIcon className='h-5 w-5' /><div>Custom Feed</div></div>}
                     rightItem={
                         <TopicDialogButton />
                     }
