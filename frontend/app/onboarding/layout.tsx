0828d8041edd074c5d0f0f5d49f8352e39f982c0
{cocoide} <cocoide@gmail.com>
2023-11-15 13:50:49 +0900

    update(FE): feat layout

--- frontend/app/onboarding/layout.tsx
+++ frontend/app/onboarding/layout.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 
 const OnboardingLayout = ({ children }: ChildrenProps) => {
     return (
-        <div className="flex lg:flex-row text-custom">
-            <div className="hidden lg:flex flex-col space-y-3 lg:w-[50%]">
+        <div className="flex lg:flex-row text-custom justify-center w-full p-10 space-x-10">
+            <div className="hidden lg:flex flex-col space-y-3 lg:w-[50%] items-center">
                 <div className="text-2xl font-bold">Tech Guideへようこそ</div>
                 <Image src={"/about/view.png"} alt='view' width={400} height={200} className="rounded-md h-[200px] w-[350px]  custom-border"></Image>
             </div>
-            <div className='border-[0.5px] border-cyan-300 bg-cyan-50 rounded-xl shadow-sm m-10'
+            <div className='custom-boarder bg-gray-50 rounded-xl shadow-sm'
             >{children}</div>
         </div>
     )
