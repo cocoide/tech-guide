ecf07c8c9022ea10700da2a618d3b69b471314b1
{cocoide} <cocoide@gmail.com>
2023-11-15 18:02:57 +0900

    style(FE)

--- frontend/app/onboarding/_components/SelectTopicGroup.tsx
+++ frontend/app/onboarding/_components/SelectTopicGroup.tsx
@@ -14,7 +14,7 @@ const SelectTopicGroup = ({ categories }: { categories?: Category[] }) => {
                 <button key={category.id} onClick={() => handleFollowCategory(category)}
                     className={clsx(followingTopics[category.id] ? "bg-cyan-300 text-white" : "bg-cyan-50 text-cyan-300 ", "relative custom-border aspect-square w-full rounded-xl flex items-center justify-center p-5")}
                 >{category.name}
-                    {followingTopics[category.id].length > 0}
+                    {followingTopics[category.id]?.length > 0}
                     <div className="absolute bottom-3 right-3">{followingTopics[category.id].length}件フォロー中</div>
                 </button>
             ))}
