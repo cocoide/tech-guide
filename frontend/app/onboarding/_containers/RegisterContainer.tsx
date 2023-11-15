2074ac939b07b10a4167394c88cb00de6f2af0b2
{cocoide} <cocoide@gmail.com>
2023-11-15 15:45:21 +0900

    update(FE): feat layout

--- frontend/app/onboarding/_containers/RegisterContainer.tsx
+++ frontend/app/onboarding/_containers/RegisterContainer.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
+import CircleLoading from '@/app/_components/animations/CircleLoading';
 import { api } from '@/app/_functions/API';
 import { useRouter } from 'next/navigation';
 
@@ -28,8 +29,14 @@ export default function RegisterContainer() {
     };
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+            {isLoading && (
+                <div className="z-10 bg-white/30 backdrop-blur-[2px]  fixed inset-0 flex justify-center items-center">
+                    <CircleLoading />
+                </div>
+            )}
             <input {...register('display_name')} />
             <input {...register('avatar_url')} />
+            <button className="">登録</button>
         </form>
     )
 }
