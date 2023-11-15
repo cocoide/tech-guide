18e80373fa3ceebc79520ede90f2c0b5906e03d6
{cocoide} <cocoide@gmail.com>
2023-11-15 16:35:43 +0900

    style(FE)

--- frontend/app/onboarding/_containers/TopicSelectContainer.tsx
+++ frontend/app/onboarding/_containers/TopicSelectContainer.tsx
@@ -6,7 +6,7 @@ export default async function TopicSelectContainer() {
     return (
         <HStack className='custom-text space-y-3 items-center w-full p-5 md:p-10'>
             <div className="">フォローするカテゴリを選ぶ</div>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
                 {categories?.map(category => (
                     <div key={category.id} className="aspect-square w-full custom-border bg-cyan-50 text-cyan-300 rounded-xl flex items-center justify-center p-5"
                     >{category.name}</div>
