5037d813c541240f2d602327d32bd4f60974ec81
{cocoide} <cocoide@gmail.com>
2023-11-15 15:57:35 +0900

    style(FE)

--- frontend/app/onboarding/_containers/SignupContainer.tsx
+++ frontend/app/onboarding/_containers/SignupContainer.tsx
@@ -17,7 +17,7 @@ const SignupContainer = () => {
     }
 
     return (
-        <div className="flex lg:flex-row flex-col text-custom lg:justify-center items-center w-full p-10 space-x-10 lg:space-y-10">
+        <div className="flex lg:flex-row flex-col text-custom lg:justify-center items-center w-full p-10 lg:space-x-10 space-y-10">
             <div className="flex flex-col space-y-3 lg:w-[50%] items-center">
                 <div className="text-2xl font-bold">Tech Guideへようこそ</div>
                 <Image src={"/about/view.png"} alt='view' width={400} height={200} className="rounded-md h-[200px] w-[350px]  custom-border"></Image>
