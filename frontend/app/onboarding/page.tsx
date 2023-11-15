93e7b4f9977c0edd635eb3dec8360fb991e3a4cd
{cocoide} <cocoide@gmail.com>
2023-11-15 16:27:12 +0900

    style(FE)

--- frontend/app/onboarding/page.tsx
+++ frontend/app/onboarding/page.tsx
@@ -1,5 +1,6 @@
 import RegisterContainer from './_containers/RegisterContainer';
 import SignupContainer from './_containers/SignupContainer';
+import TopicSelectContainer from './_containers/TopicSelectContainer';
 
 interface Porps {
     searchParams: { "step": string }
@@ -10,6 +11,9 @@ export default async function OnboardingPage({ searchParams }: Porps) {
         <div className="w-full">
             {step === 2 ?
                     <RegisterContainer />
+                    :
+                step === 3 ?
+                    <TopicSelectContainer />
                     :
                 <SignupContainer />
             }
