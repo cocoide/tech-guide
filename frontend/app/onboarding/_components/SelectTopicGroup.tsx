b2910e96b611c9ef7d99e34c2fb96c14504338fd
{cocoide} <cocoide@gmail.com>
2023-11-15 18:16:26 +0900

    style(FE)

--- frontend/app/onboarding/_components/SelectTopicGroup.tsx
+++ frontend/app/onboarding/_components/SelectTopicGroup.tsx
@@ -2,20 +2,26 @@
 
 import { Category, Topic } from '@/types/model';
 import { clsx } from '@/utils/clsx';
+import { useState } from 'react';
 
 const SelectTopicGroup = ({ categories }: { categories?: Category[] }) => {
-    const followingTopics: Record<number, Topic[]> = {};
+    const [followingTopics, setFollowingTopics] = useState<Record<number, Topic[]>>()
     function handleFollowCategory(category: Category) {
-        followingTopics[category.id] = category.topics
+        setFollowingTopics(prevTopics => ({
+            ...prevTopics,
+            [category.id]: category.topics
+        }));
     }
     return (
         <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
             {categories?.map(category => (
                 <button key={category.id} onClick={() => handleFollowCategory(category)}
-                    className={clsx(followingTopics[category.id] ? "bg-cyan-300 text-white" : "bg-cyan-50 text-cyan-300 ", "relative custom-border aspect-square w-full rounded-xl flex items-center justify-center p-5")}
+                    className={clsx(followingTopics && followingTopics[category.id] ? "bg-cyan-300 text-white" : "bg-cyan-50 text-cyan-300 ", "relative custom-border aspect-square w-full rounded-xl flex items-center justify-center p-5")}
                 >{category.name}
-                    {followingTopics[category.id]?.length > 0}
-                    <div className="absolute bottom-3 right-3">{followingTopics[category.id]?.length ?? 0}件フォロー中</div>
+                    {followingTopics && followingTopics[category.id]?.length > 0
+                        &&
+                        <div className="absolute bottom-3 right-3">{followingTopics[category.id]?.length}件のトピック</div>
+                    }
                 </button>
             ))}
         </div>
